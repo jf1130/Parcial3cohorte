eacca7fc4b5e9eecfee42632a6eb72235af8edfa
jf1130 <jfmp1130@gmai1.com>
2022-05-24 20:50:08 -0500

    CodigoParcial

---
+++ Jugador.py
@@ -0,0 +1,6 @@
+
+import Persona from Persona
+
+def __init__(self, Nombre:str,nif:Nif, fechaNac:Fecha, numFed: int):
+    Persona.__init__(self, Nombre, nif, fechaNac, numFed)
+    self.numFed= numFed
---
+++ Persona.py
@@ -0,0 +1,5 @@
+
+def __init__(self, Nombre:str,nif:Nif, fechaNac:Fecha):
+     self.Nombre=Nombre
+     self.nif= Nif
+     self.fechaNac=Fecha
